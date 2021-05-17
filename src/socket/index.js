d47e73eebeb7987dcb572ef43c7d6e9f14d27d2f
RomainMaillot <romainmaillot.rm@gmail.com>
2021-05-17 15:50:42 +0200

    change back uri

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 class Socket {
   constructor() {
-    this.socket = io('https://euclid-back.herokuapp.com:9000')
+    this.socket = io('https://euclid-back.herokuapp.com')
     this.roomKey = null
     this.userId = null
     console.log('Connection')
